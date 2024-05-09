ca1906836ad048c4f8127edf78013079b15ef6a5
Jose Espinosa-Carrasco <kadomu@gmail.com>
2024-05-09 11:36:13 +0200

    Update bin/fetch_oma_by_sequence.py

--- bin/fetch_oma_by_sequence.py
+++ bin/fetch_oma_by_sequence.py
@@ -15,7 +15,6 @@ def main() -> None:
         raise ValueError("Not enough arguments. Usage: fetch_oma_by_sequence.py <fasta> <id_out> <taxid_out> <exact_out>")
 
     seqs = SeqIO.parse(sys.argv[1], "fasta")
-
     seq = next(seqs).seq
 
     # Only use the first sequence, ignore all others
