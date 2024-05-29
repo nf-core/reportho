b551ee127520ec4373312c2e831a627b7df9a6ec
itrujnara <itrujnara@wp.pl>
2024-05-29 12:30:21 +0200

    Improved isoform handling in OMA API calls

--- bin/fetch_oma_groupid.py
+++ bin/fetch_oma_groupid.py
@@ -5,7 +5,7 @@
 
 import sys
 
-from utils import fetch_seq
+from utils import fetch_seq, safe_get
 
 
 def main() -> None:
@@ -28,13 +28,13 @@ def main() -> None:
     # If main isoform not found, check the first alternative isoform
     if entry == dict():
         if len(json["alternative_isoforms_urls"]) > 0:
-            isoform = json["alternative_isoforms_urls"][0]
-            success, json = fetch_seq(isoform)
-            if not success:
-                raise ValueError("Isoform fetch failed, aborting")
-            if json["is_main_isoform"]:
-                entry = json
-            else:
+            res = safe_get(json["isoforms"])
+            json2 = res.json()
+            for isoform in json2:
+                if isoform["is_main_isoform"]:
+                    entry = isoform
+                    break
+            if entry == dict():
                 raise ValueError("Isoform not found")
     print(entry['oma_group'])
 
