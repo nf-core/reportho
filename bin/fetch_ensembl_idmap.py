f0dd1a4207f67c637e6337e018dd51f80473c871
itrujnara <itrujnara@wp.pl>
2025-04-05 15:32:52 +0200

    Add documentation to sequence fetch scripts

--- bin/fetch_ensembl_idmap.py
+++ bin/fetch_ensembl_idmap.py
@@ -3,6 +3,8 @@
 # Written by Igor Trujnara, released under the MIT license
 # See https://opensource.org/license/mit for details
 
+# This script fetches Ensembl species identifiers and their NCBI taxon IDs for the Ensembl API.
+
 import requests
 
 
